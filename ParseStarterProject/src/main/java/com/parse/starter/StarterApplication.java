9eead75354a1db6cbb777a8b6d48fe600be36b8f
PhilipChakram <philip.chakram@gmail.com>
2017-12-27 14:25:02 -0500

    Second comit

--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -9,14 +9,9 @@
 package com.parse.starter;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 
 public class StarterApplication extends Application {
@@ -30,9 +25,9 @@ public class StarterApplication extends Application {
 
     // Add your initialization code here
     Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-            .applicationId("abe1c94b00ba6aa6b8e013b2de77dc5bd9bd5e9b")
-            .clientKey("4a3064172bba51283371645ac21a45e82c588c2a")
-            .server("http://ec2-54-149-166-193.us-west-2.compute.amazonaws.com/parse/")
+            .applicationId("")
+            .clientKey("")
+            .server("")
             .build()
     );
 
